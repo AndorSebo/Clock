e29d9b583a3600e9f071ec3e088ded9d0bec3c44
AndorSebo <andorsebo@gmail.com>
2018-11-07 09:09:23 +0100

    Clock view modified.

--- Clock/app/src/main/java/hu/andorsebo/clock/MainActivity.java
+++ Clock/app/src/main/java/hu/andorsebo/clock/MainActivity.java
@@ -1,13 +1,25 @@
 package hu.andorsebo.clock;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Calendar;
+import com.turki.vectoranalogclockview.VectorAnalogClock;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR,-1);
+        VectorAnalogClock clock = findViewById(R.id.clock);
+        clock.setCalendar(calendar)
+                .setDiameterInDp(400.0f)
+                .setOpacity(1.0f)
+                .setShowSeconds(true)
+                .setColor(Color.BLACK);
     }
 }
